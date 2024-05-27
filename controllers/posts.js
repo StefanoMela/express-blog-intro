4ad7406bf8e36a990d3b448073dc7e2ecdd550ca
StefanoMela <stefano.mela25@gmail.com>
2024-05-27 14:11:01 +0200

    feat json posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,7 @@
 // imports
-const { json } = require('express')
 const postsArray = require('../data/post-array.js')
 const postsJSON = require('../data/posts.json')
-const {readJSON, writeJSON} = require('../utils.js');
+const { writeJSON } = require('../utils.js');
 
 
 // export controller
@@ -11,9 +10,9 @@ module.exports = {
         res.format({
             html: () => {
                 let html = '<div>'
-                postsArray.forEach( post => {
-                    html += 
-                    `<img width="250" src="/${post.immagine}"/>
+                postsArray.forEach(post => {
+                    html +=
+                        `<img width="250" src="/${post.immagine}"/>
                     <h2>${post.titolo}</h2>
                     <p>${post.contenuto}
                     <h3>tags:</h3>
@@ -22,6 +21,17 @@ module.exports = {
                     post.tags.forEach(tag => html += `<li>${tag}</li>`);
                     html += `</ul>`
                 });
+                postsJSON.forEach(post => {
+                    html +=
+                        `<img width="250" src="/${post.immagine}"/>
+                    <h2>${post.titolo}</h2>
+                    <p>${post.contenuto}
+                    <h3>tags:</h3>
+                    <ul>
+                    `;
+                    post.tags.forEach(tag => html += `<li>${tag}</li>`);
+                    html += `</ul>`
+                })
                 html += `</div>`
                 res.send(html)
             },
